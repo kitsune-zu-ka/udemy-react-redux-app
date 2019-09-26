5cc8feee8bd0df4f06e64dd225d79177d7a4f53a
yuya <ktmk.sch@gmail.com>
2019-09-26 11:27:47 +0900

    section 3-14

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-    render() {
-        return (
-          <h1>Hello world!</h1>
-        );
-    }
+// class App extends Component {
+//     render() {
+//         return (
+//             <React.Fragment>
+//                 <label htmlFor="bar">bar</label>
+//                 <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//             </React.Fragment>
+//         );
+//     }
+// }
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Cat/>
+            <Cat/>
+            <Cat/>
+            <Cat/>
+        </React.Fragment>
+    );
+}
+
+const Cat = () => {
+    return <div>Meow!</div>
 }
 
 export default App;
